e4148085795fdfc22161ca762df5acb88df41cd9
noel-ellis <noel_ellis@proton.me>
2023-08-11 16:49:04 +0200

    split validate_encoded_field into two separate validatiors: original and validate_non_empty

--- core/models.py
+++ core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from .validators import validate_encoded_field
+from .validators import validate_encoded_field, validate_non_empty
 
 class Folder(models.Model):
-    name = models.CharField(max_length=256, blank=False, validators=[validate_encoded_field])
+    name = models.CharField(max_length=256, blank=False, validators=[validate_encoded_field, validate_non_empty])
     account = models.ForeignKey('account.Account', on_delete=models.CASCADE, limit_choices_to={"is_active": True})
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, default=None)
--- core/validators.py
+++ core/validators.py
@@ -4,10 +4,12 @@ from binascii import Error as DecodingError
 
 # todo: put the encryption algorithm in a config so that it can be changed later without rewriting a lot of code
 def validate_encoded_field(value):
-    if value.strip() != value:
-        raise ValidationError('data is empty')
     try:
         base64.b64decode(value)
     except DecodingError:
         raise ValidationError('data is corrupted')
+    
+def validate_non_empty(value):
+    if value.strip() != value:
+        raise ValidationError('no data')
     
