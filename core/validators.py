53b52faeb5e9c76fcc651a00be77d9fbc45b9329
noel-ellis <noel_ellis@proton.me>
2023-08-11 15:27:36 +0200

    removed unused validators

--- core/validators.py
+++
@@ -1,6 +0,0 @@
-from django.core.exceptions import ValidationError
-
-def validate_name(value):
-    if value.strip() != value:
-        raise ValidationError('Tag cannot be empty or contain whitespaces at the ends')
-    
