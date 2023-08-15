2395f794fb845f58ef359e5c8aa90b19fea85d4f
noel-ellis <noel_ellis@proton.me>
2023-08-15 18:19:27 +0200

    removed unused modules in core.tests.tests

--- core/tests/tests.py
+++ core/tests/tests.py
@@ -1,5 +1,4 @@
 from django.test import TestCase
-from django.db.utils import DataError, IntegrityError
 from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 
